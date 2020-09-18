ee7b57278141e014c97ba513fbc8b79bb828900b
minhee0101 <67231799+minhee0101@users.noreply.github.com>
2020-09-19 00:14:35 +0900

    5P12

---
+++ 05_practice_2.ino
@@ -0,0 +1,27 @@
+#define PIN_LED 7
+int i;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(7, OUTPUT); //7번핀을 출력핀으로 설정
+  }
+
+void loop(){ 
+  digitalWrite(7,0);
+  delay(1000);
+  for(i=0; i<11; i++)
+  {
+    digitalWrite(7,!(digitalRead(7)));
+    delay(200);
+  }
+  i++;
+  while(1) //infinite loop
+  {
+    if(i>12)
+    {
+      digitalWrite(7,0);
+      break;
+    }
+    
+  }
+  
+}
